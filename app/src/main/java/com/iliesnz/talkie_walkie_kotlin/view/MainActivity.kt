b67b09673584d5033eecdbab5d8017af1f3bea6f
Ilies <Ili.narenzozanzan@gmail.com>
2026-07-19 09:44:45 +0300

    - Organisation de l'architecture en MVVM. - Vue de base.

---
+++ app/src/main/java/com/iliesnz/talkie_walkie_kotlin/view/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.iliesnz.talkie_walkie_kotlin.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.iliesnz.talkie_walkie_kotlin.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val confirmation = findViewById<Button>(R.id.button)
+        val IP = findViewById<EditText>(R.id.IP)
+        val chargement = findViewById<ProgressBar>(R.id.chargement)
+
+        val talkieIntent = Intent(this, Talkie::class.java)
+
+        confirmation.setOnClickListener {
+            if (IP.text.toString().isEmpty()){
+                Toast.makeText(this, "Il manque l'ip du serveur !", Toast.LENGTH_LONG).show()
+            }
+            else{
+                Toast.makeText(this, "Tentative de connexion...", Toast.LENGTH_LONG).show()
+                IP.visibility = View.GONE
+                confirmation.visibility = View.GONE
+                chargement.visibility = View.VISIBLE
+
+                startActivity(talkieIntent)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/iliesnz/talkie_walkie_kotlin/view/Talkie.kt
@@ -0,0 +1,53 @@
+package com.iliesnz.talkie_walkie_kotlin.view
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.iliesnz.talkie_walkie_kotlin.R
+
+class Talkie : AppCompatActivity() {
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_talkie)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val back = findViewById<Button>(R.id.button_back)
+        val talk = findViewById<Button>(R.id.button_talk)
+
+        val homeIntent = Intent(this, MainActivity::class.java)
+
+        back.setOnClickListener {
+            startActivity(homeIntent)
+        }
+
+        talk.setOnTouchListener { view, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    Toast.makeText(this, "Parler...", Toast.LENGTH_LONG).show()
+                    true
+                }
+
+                MotionEvent.ACTION_UP -> {
+                    Toast.makeText(this, "Stop...", Toast.LENGTH_LONG).show()
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+    }
+}
