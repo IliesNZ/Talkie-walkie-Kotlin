78f3bc910959494b00b941142762d83ff138e994
IliesNZ <Ili.narenzozanzan@gmail.com>
2026-09-11 01:20:28 +0200

    autorisation du microphone

--- app/src/main/java/com/iliesnz/talkie_walkie_kotlin/view/HomeView.kt
+++ app/src/main/java/com/iliesnz/talkie_walkie_kotlin/view/HomeView.kt
@@ -1,6 +1,8 @@
 package com.iliesnz.talkie_walkie_kotlin.view
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -8,7 +10,9 @@ import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.Lifecycle
@@ -28,6 +32,14 @@ class HomeView : AppCompatActivity() {
     private lateinit var talkieViewIntent: Intent
     private lateinit var homeViewmodel: HomeViewmodel
 
+    private val requestMicrophonePermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                confirmation.isEnabled = true
+            } else {
+                Toast.makeText(this, "La permission du microphone est requise pour utiliser l'application.", Toast.LENGTH_LONG).show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +59,8 @@ class HomeView : AppCompatActivity() {
         ipAddress = findViewById<EditText>(R.id.IP)
         chargement = findViewById<ProgressBar>(R.id.chargement)
 
+        confirmation.isEnabled = false
+
         talkieViewIntent = Intent(this, TalkieView::class.java)
 
         confirmation.setOnClickListener {
@@ -61,7 +75,26 @@ class HomeView : AppCompatActivity() {
             }
         }
 
+        checkAndRequestMicrophonePermission()
 
+    }
+
+    private fun checkAndRequestMicrophonePermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) == PackageManager.PERMISSION_GRANTED -> {     // Permission déjà accepté
+                confirmation.isEnabled = true
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO) -> {     // Si l'utilisateur refuse
+                Toast.makeText(this, "La permission du microphone est requise pour utiliser l'application.", Toast.LENGTH_LONG).show()
+                requestMicrophonePermission.launch(Manifest.permission.RECORD_AUDIO)
+            }
+            else -> {       // Première demande de permission
+                requestMicrophonePermission.launch(Manifest.permission.RECORD_AUDIO)
+            }
+        }
     }
 
     private fun uiState(state: HomeUiState) {
